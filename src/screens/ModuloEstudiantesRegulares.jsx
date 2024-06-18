f47166412bb82484bed5044c37d3e35f2eefe336
judithmarg <judithmargaritapacollalli@gmail.com>
2024-06-17 22:56:05 -0400

    barra de navegación

---
+++ src/screens/ModuloEstudiantesRegulares.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Layout from "./Layout";
+
+const EstudiantesRegulares = () => {
+    return (
+        
+            <h1>Estudiantes Regulares</h1>
+        
+    );
+}
+export default EstudiantesRegulares;
