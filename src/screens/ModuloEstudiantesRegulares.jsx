26160bcd8de25aa64370b0a2296c6ef8dace763e
judithmarg <judithmargaritapacollalli@gmail.com>
2024-06-20 02:12:19 -0400

    el menu agregado

--- src/screens/ModuloEstudiantesRegulares.jsx
+++ src/screens/ModuloEstudiantesRegulares.jsx
@@ -1,11 +1,45 @@
 import { useState } from "react";
 import Layout from "./Layout";
+import DropdownMenu from "../utils/DropdownMenu";
+import { Button } from "@mui/material";
+
+function DropdownItem({ text }) {
+    const [isPressed, setIsPressed] = useState(false);
+    return (
+        <Button style={{padding:4, textTransform:'none', justifyContent: 'left',color:'black'}} onClick={()=>{setIsPressed(true)}}>
+            {text}
+        </Button>
+    );
+}
 
 const EstudiantesRegulares = () => {
     return (
-        
-            <h1>Estudiantes Regulares</h1>
-        
+        <>
+        <div style={{marginTop:'1.5rem'}}>
+            <DropdownMenu label="Inscripciones">
+                <DropdownItem text="Fechas" />
+                <DropdownItem text="Requisitos" />
+                <DropdownItem text="Proceso" />
+            </DropdownMenu>
+            <DropdownMenu label="Trámites">
+                <DropdownItem text="Auxiliaturas" />
+                <DropdownItem text="Cambio de carrera" />
+                <DropdownItem text="Solicitud para trabajo dirigido" />
+                <DropdownItem text="Registro de tema de perfil" />
+                <DropdownItem text="Defensa pública" />
+            </DropdownMenu>
+            <DropdownMenu label="Documentos">
+                <DropdownItem text="Guías de elaboración" />
+                <DropdownItem text="Certificaciones de solvencia" />
+                <DropdownItem text="Titulación" />
+            </DropdownMenu>
+            <DropdownMenu label="Proyectos de fin de carrera">
+                <DropdownItem text="Banco de temas disponibles" />
+                <DropdownItem text="Buscador de proyectos" />
+                <DropdownItem text="Convocatorias" />
+            </DropdownMenu>
+            </div>
+        </>
     );
 }
 export default EstudiantesRegulares;
